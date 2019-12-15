1d5c37f5b1d27e923c63b6cd4109a20f0bb2296f
Paul Glass <pnglass@gmail.com>
2019-12-15 01:21:14 -0600

    Drop support for Python 2

--- setup.py
+++ setup.py
@@ -27,7 +27,6 @@ setup_params = dict(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
